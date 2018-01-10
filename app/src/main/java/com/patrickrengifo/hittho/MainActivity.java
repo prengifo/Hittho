bf1c1b0fb4ecb744502c32f5c36ab4883fbd3e38
Patrick Rengifo <patrick.rengifo@gmail.com>
2018-01-09 21:43:47 -0400

    Default xml replaced with Litho component

--- app/src/main/java/com/patrickrengifo/hittho/MainActivity.java
+++ app/src/main/java/com/patrickrengifo/hittho/MainActivity.java
@@ -1,13 +1,26 @@
 package com.patrickrengifo.hittho;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.facebook.litho.ComponentContext;
+import com.facebook.litho.LithoView;
+import com.facebook.litho.widget.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        final ComponentContext c = new ComponentContext(this);
+
+        final LithoView lithoView = LithoView.create(
+                this /* context */,
+                Text.create(c)
+                        .text("Hello, World!")
+                        .textSizeDip(16)
+                        .build());
+
+        setContentView(lithoView);
     }
 }
